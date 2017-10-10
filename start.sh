d0aa92dac59e89b28bbce6e4b3fcb92b87bc571f
evatlsong <evatlsong@gmail.com>
2017-10-10 16:36:53 +0800

    change docker image

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker container run -p 4000:80 --name hexo-server -d -v "$PWD"/source:/Hexo/source -v "$PWD"/themes:/Hexo/themes -v "$PWD"/_config.yml:/Hexo/_config.yml evatlsong/hexo:6.10 s
+docker container run -p 4000:80 --name hexo-server -d -v "$PWD"/source:/Hexo/source -v "$PWD"/themes:/Hexo/themes -v "$PWD"/_config.yml:/Hexo/_config.yml evatlsong/hexo:3.3.9-alpine s
